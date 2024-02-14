f78f7207ca9459e79fdfd87e5419a5a584b3ab08
M1rtex <themirtex@gmail.com>
2024-02-14 18:00:16 +0500

    ml-kit eng only ocr sample

--- NotepadAI/app/components/OCR.js
+++ NotepadAI/app/components/OCR.js
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View, Button, Image } from 'react-native'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import * as ImagePicker from 'expo-image-picker';
+import MlkitOcr from 'react-native-mlkit-ocr';
 
 
 export default function OCR() {
     const [image, setImage] = useState(null);
+    const [text, setText] = useState("");
+
+    const getText = async () => {
+      let resultFromUri = await MlkitOcr.detectFromUri(image);
+      console.log(resultFromUri[0].text)
+      setText(resultFromUri[0].text)
+    }
 
     const pickImage = async () => {
       let result = await ImagePicker.launchImageLibraryAsync({
@@ -19,11 +27,13 @@ export default function OCR() {
         setImage(result.assets[0].uri);
       }
     };
-  
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        {image ? <Button title="Use OCR" onPress={getText} /> : null}
+        {image && <Image source={{ uri: image }} style={{width: 400, height: 200}} />}
+        <Text style={{backgroundColor: "red"}}>{text}</Text>
       </View>
     )
 }
