c9d5dac352f238190a22554a4f403184f92d6cf9
M1rtex <themirtex@gmail.com>
2024-02-14 18:38:32 +0500

    te-ocr-lib integrate 1

--- NotepadAI/app/components/OCR.js
+++ NotepadAI/app/components/OCR.js
@@ -1,23 +1,46 @@
 import { StyleSheet, Text, View, Button, Image } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import * as ImagePicker from 'expo-image-picker';
-import MlkitOcr from 'react-native-mlkit-ocr';
+import {
+  getText,
+  DataInputType,
+  OcrEngineMode,
+  OCREvent,
+  useOCREventListener,
+} from 'rn-ocr-lib';
 
 
 export default function OCR() {
     const [image, setImage] = useState(null);
     const [text, setText] = useState("");
 
-    const getText = async () => {
-      let resultFromUri = await MlkitOcr.detectFromUri(image);
-      console.log(resultFromUri)
-      let text_ = ""
-      resultFromUri.forEach((bounding) => {
-        text_ += bounding.text + "\n"
-      })
-      console.log(text_)
-      setText(text_)
+    const extractText = async () => {
+      let uri = image
+      try {
+        getText(uri.replace('file://', ''), DataInputType.file, {
+          ocrEngineMode: OcrEngineMode.ACCURATE,
+          lang: ['rus', 'eng'],
+        });
+      } catch (err) {
+        setError(err.message);
+      }
     }
+
+    useOCREventListener((event, data) => {
+      switch (event) {
+        case OCREvent.FINISHED:
+          setText(data.text);
+          return;
+        case OCREvent.PROGRESS:
+          setProgress(data.percent.toString() + "%");
+          return;
+        case OCREvent.ERROR:
+          setError(data.error);
+          return;
+        default:
+          return;
+      }
+    });
 
     const pickImage = async () => {
       let result = await ImagePicker.launchImageLibraryAsync({
@@ -36,7 +59,7 @@ export default function OCR() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {image ? <Button title="Use OCR" onPress={getText} /> : null}
+        {image ? <Button title="Use OCR" onPress={extractText} /> : null}
         {image && <Image source={{ uri: image }} style={{width: 400, height: 200}} />}
         <Text style={{backgroundColor: "red"}}>{text}</Text>
       </View>
