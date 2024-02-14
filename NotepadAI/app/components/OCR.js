5f91b8ba5589bddce6e362c9e5cfb42d0d9d63dd
M1rtex <themirtex@gmail.com>
2024-02-14 21:47:58 +0500

    tesseract update

--- NotepadAI/app/components/OCR.js
+++ NotepadAI/app/components/OCR.js
@@ -1,69 +1,60 @@
-import { StyleSheet, Text, View, Button, Image } from 'react-native'
-import React, {useState, useEffect} from 'react'
-import * as ImagePicker from 'expo-image-picker';
-import {
-  getText,
-  DataInputType,
-  OcrEngineMode,
-  OCREvent,
-  useOCREventListener,
-} from 'rn-ocr-lib';
-
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
+import TesseractOcr, {
+  LANG_ENGLISH,
+  LANG_RUSSIAN,
+} from "react-native-tesseract-ocr";
 
 export default function OCR() {
-    const [image, setImage] = useState(null);
-    const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
+  const [text, setText] = useState("");
 
-    const extractText = async () => {
-      let uri = image
-      try {
-        getText(uri.replace('file://', ''), DataInputType.file, {
-          ocrEngineMode: OcrEngineMode.ACCURATE,
-          lang: ['rus', 'eng'],
-        });
-      } catch (err) {
-        setError(err.message);
-      }
+  const recognizeTextFromImage = async (path) => {
+    console.log("Recognize Started");
+    try {
+      const tesseractOptions = {};
+      const recognizedText = await TesseractOcr.recognize(
+        path,
+        [LANG_ENGLISH, LANG_RUSSIAN],
+        tesseractOptions
+      );
+      setText(recognizedText);
+      console.log(recognizedText);
+    } catch (err) {
+      console.error(err);
+      setText("");
     }
+  };
 
-    useOCREventListener((event, data) => {
-      switch (event) {
-        case OCREvent.FINISHED:
-          setText(data.text);
-          return;
-        case OCREvent.PROGRESS:
-          setText(data.percent.toString() + "%");
-          return;
-        case OCREvent.ERROR:
-          setText(data.error);
-          return;
-        default:
-          return;
-      }
+  const extractText = async () => {
+    await recognizeTextFromImage(image);
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
     });
 
-    const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        quality: 1,
-      });
-  
-      console.log(result.assets);
-  
-      if (!result.canceled) {
-        setImage(result.assets[0].uri);
-      }
-    };
+    console.log("Image picked");
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {image ? <Button title="Use OCR" onPress={extractText} /> : null}
-        {image && <Image source={{ uri: image }} style={{width: 400, height: 200}} />}
-        <Text style={{backgroundColor: "red"}}>{text}</Text>
-      </View>
-    )
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image ? <Button title="Use OCR" onPress={extractText} /> : null}
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 400, height: 200 }} />
+      )}
+      <Text style={{ backgroundColor: "red" }}>{text}</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
