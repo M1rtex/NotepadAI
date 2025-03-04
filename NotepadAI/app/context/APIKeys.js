6162b805bc56a6e14091d58575bbacd49301bcce
M1rtex <75471608+M1rtex@users.noreply.github.com>
2025-03-04 16:34:17 +0500

    minor fix

--- NotepadAI/app/context/APIKeys.js
+++ NotepadAI/app/context/APIKeys.js
@@ -1,3 +1,3 @@
 
-export const GoogleAPIKey = "AIzaSyCFjpRIaHx0SQ3_owrswUTabIdOTuOitgE"
-export const TogetherAPIKey = "ced5adc36ab877527b6e709897c3e0dcc4727167979388071f4d3b5578760947"
+export const GoogleAPIKey = ""
+export const TogetherAPIKey = ""
